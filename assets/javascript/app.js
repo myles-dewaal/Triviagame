b11f0ac620f1351f1ee3a49b2097200eb2192b46
myles-dewaal <mylesdewaal@hotmail.com>
2019-09-18 22:27:33 -0600

    redoing javascript as my first one was not working properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,29 @@
-function check(){
-var question1 = document.quiz.question1.value;
-var question2 = document.quiz.question2.value;
-var question3 = document.quiz.question3.value;
-var question4 = document.quiz.question4.value;
-var question5 = document.quiz.question5.value;
-var score = 0
-if (question1 === "1964") {
-    score++
-}
-if (question2 === "93") {
-    score++
-}
-if (question3 === "July 17, 1955") {
-    score++
-}
-if (question4 === "Blastiose") {
-    score++
-}
-if (question5 === "114.4 Billion") {
-    score++
-}
-$("#score").text(score + "/5")
-
-}
+var questions = [
+    {
+        question: "What year was the company Nike founded?",
+        answers: ["1987", "1980", "1964", "1973"],
+        correct: "1964"
+    },    {
+        question: "What year was the company Nike founded?",
+        answers: ["1987", "1980", "1964", "1973"],
+        correct: "1964"
+    },
+]
 $(document).ready(function () {
-    var i = 60;
+    $(".submit-button").on("click", function (event) {
+        event.preventDefault()
+        console.log(this)
+    })
+    // $("#after_submit").style.visibility = "visible";
+    // $("#number_correct").innerHTML = "You got " + correct + " correct.";
+    var i = 3;
     $(".timeremaining").text(i);
     setInterval(function () {
-        if(i==0){alert ("Times Up");}
-        i--;
         $(".timeremaining").text(i > -1 ? i : 0);
+        i--;
     }, 1000);
+    if (i === 0) { (".timeremaining").text("Times Up"); }
+
 
 });
 
